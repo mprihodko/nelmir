e13ff5c48c7da229663c584cddaff10160e5b0b7
dv <emarldmihail@gmail.com>
2016-04-17 17:50:22 +0200

    add sidebars

--- wp-content/themes/Avada-Child-Theme/corefunctions/functinscore.php
+++ wp-content/themes/Avada-Child-Theme/corefunctions/functinscore.php
@@ -27,3 +27,17 @@ function register_my_widgetzz2(){
 	) );
 }
 add_action( 'widgets_init', 'register_my_widgetzz2' );
+
+function register_my_widgetzz3(){
+	register_sidebar( array(
+		'name' => 'Category_sidebar',
+		'id' => "category_sidebar",
+		'description' => '',
+		'class' => '',
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget' => "</li>\n",
+		'before_title' => '<h2 class="widgettitle">',
+		'after_title' => "</h2>\n",
+	) );
+}
+add_action( 'widgets_init', 'register_my_widgetzz3' );
